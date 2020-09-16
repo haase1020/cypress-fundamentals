5751d433cd06cadcc089f3a873a9dc2521d1253c
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-16 04:31:33 -0400

    beforeEach example

---
+++ cypress/integration/before_each.spec.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+//from linkedin course// alias version of max_char_input.spec.js
+
+//max chars is 15
+describe('Text box with max chars', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/example-2');
+
+    cy.get('[data-cy="last-name-chars-left-count]').as('charsLeftSpan');
+  });
+  it('displays the appropriate remaining char count', () => {
+    //eq is if you have more than one item
+    cy.get('@charsLeftSpan').invoke('text').should('equal', '15');
+
+    cy.get('input').type('hello');
+
+    cy.get('@charsLeftSpan').invoke('text').should('equal', '10');
+
+    cy.get('input').eq(1).type(' my friend');
+    cy.get('[data-cy="last-name-chars-left-count]')
+      .invoke('text')
+      .should('equal', '0');
+  });
+
+  it('prevents user from typing over 15 chars', () => {
+    cy.get('input').eq(1).type('abcdefghijklmnopqurstuvwxyz');
+    cy.get('input').eq(1).should('have.attr', 'value', 'abcdefghijklmno');
+  });
+});
