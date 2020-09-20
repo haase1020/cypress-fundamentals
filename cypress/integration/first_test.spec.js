99227ac933f08aa03ff4c0914477ddbab9dc5185
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-20 12:34:34 -0400

    working on Cypress Web Automation course

--- cypress/integration/first_test.spec.js
+++ cypress/integration/first_test.spec.js
@@ -11,9 +11,45 @@ describe('Heading text', () => {
     navigate();
   });
 
-  it('containes the correct title', () => {
+  it('contains the correct title', () => {
     cy.get('h1').invoke('text').should('equal', 'todos');
   });
-});
+  it.only('then and wrap methods', () => {
+    cy.visit('/');
+    cy.contains('Forms').click();
+    cy.contains('Forms Layouts').click();
 
-it();
+    cy.contains('nb-card', 'using the Grid')
+      .find('[for="inputEmail1"]')
+      .should('contain', 'Email');
+
+    cy.contains('nb-card', 'using the Grid')
+      .find('[for="inputEmail1"]')
+      .should('contain', Password);
+
+    cy.contains('nb-card', 'Using the Grid').then((firstForm) => {
+      // this is JQuery format so can save as a constant
+      const emailLabelFirst = firstForm.find('[for=inputEmail1]').text();
+      expect(emailLabelFirst).to.equal('Email');
+    });
+  });
+
+  it.only('invoke command', () => {
+    cy.visit('/');
+    cy.contains('Forms').click();
+    cy.contains('Forms Layouts').click();
+
+    //1
+    cy.get('[for="inputEmail1"]').should('contain', 'Email address');
+    //2
+    cy.get('[for="inputEmail1"]').then((label) => {
+      expect(label.text()).to.equal('Email address');
+    });
+    //3
+    cy.get('[for="inputEmail1"]')
+      .invoke('text')
+      .then((text) => {
+        expect(text).to.equal('Email address');
+      });
+  });
+});
